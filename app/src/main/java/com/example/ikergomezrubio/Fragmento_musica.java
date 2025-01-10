03abb9ea67ad73226fff5f94fdd98881e4254ffb
iker <igraliende@gmail.com>
2025-01-10 09:54:14 +0100

    Fragmento_musica

--- app/src/main/java/com/example/ikergomezrubio/Fragmento_musica.java
+++ app/src/main/java/com/example/ikergomezrubio/Fragmento_musica.java
@@ -4,13 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 
 public class Fragmento_musica extends Fragment {
 
+    private boolean encendida;
+    private ImageView botonMusica;
+
     public Fragmento_musica() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
--- app/src/main/java/com/example/ikergomezrubio/Servicio_Musica.java
+++ app/src/main/java/com/example/ikergomezrubio/Servicio_Musica.java
@@ -2,13 +2,10 @@ package com.example.ikergomezrubio;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
-
-import java.security.Provider;
-import java.util.List;
-import java.util.Map;
 
 public class Servicio_Musica extends Service {
 
@@ -16,5 +13,30 @@ public class Servicio_Musica extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    MediaPlayer miReproductor;
+
+    public void onCreate() {
+        super.onCreate();
+        miReproductor = MediaPlayer.create(this, R.raw.CowboySong);
+        miReproductor.setLooping(true);
+        miReproductor.setVolume(100, 100);
+
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        miReproductor.start();
+        return START_STICKY;
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (miReproductor.isPlaying()) {
+            miReproductor.stop();
+        }
+
+        miReproductor.release();
+        miReproductor = null;
     }
 }
